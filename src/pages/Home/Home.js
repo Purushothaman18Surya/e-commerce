f41a684318e6ab96d27dacd214035d6d5c9176c6
Purushothaman18Surya <purushothaman18surya@gmail.com>
2023-01-31 11:31:37 +0530

    e-commerce home page

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,17 @@
+import Footer from "../../components/Shared/Footer/Footer";
+import Header from "../../components/Shared/Header/Header";
+
+
+
+function Home(){
+    return(<div>
+        
+        <Header />
+    <h1>this is Home page</h1>
+    <Footer />
+    </div>);
+}
+
+
+
+export default Home;
